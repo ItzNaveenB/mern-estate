4c7073a155ddcec54888e6fe984724aba9b77040
Naveen <naveenbaghel5429@gmail.com>
2023-11-25 17:42:06 +0530

    create header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
