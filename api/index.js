9e7fa7ec061b3d62d4d25a7ab1c82967280eb950
Naveen <naveenbaghel5429@gmail.com>
2023-11-26 21:02:27 +0530

    create User route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv";
+import UserRouter from './routes/user.route.js'
 
 const app = express();
 dotenv.config()
@@ -13,7 +14,8 @@ mongoose.connect(mongoDBUrl)
   .catch((err) => {
     console.error(`Error connecting to MongoDB: ${err}`)
   });
-// const db = mongoose.connection;
+  
+ app.use('/api/user',UserRouter);
 
 app.listen(3000, () => {
   console.log("servert is running on port 3000!")
