37f7db67d84db0aa1245edf97ce35b6fa6e0555c
Naveen <naveenbaghel5429@gmail.com>
2023-12-02 16:22:44 +0530

    make profile page private

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ mongoose.connect(mongoDBUrl)
 app.use('/api/user', UserRouter);
 app.use('/api/auth', authRouter)
 
-app.use((err, req, res, next) => {
+app.use((err, req, res, next) => { 
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
